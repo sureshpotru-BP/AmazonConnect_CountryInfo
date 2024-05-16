f5445bff45e614aad361a619c41ba15bb41796d6
sureshpotru-BP <154581462+sureshpotru-BP@users.noreply.github.com>
2024-05-16 15:07:52 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "init" {
 
 resource "aws_lambda_function" "example" {
   filename         = "${path.module}/lambda/CallBack_Test.zip"
-  function_name    = var.lambda_name
+  function_name    = "CallbackTest"
   role             = aws_iam_role.lambda_exec.arn
   handler          = "CallBack_Test.lambda_handler"
   #filename         = "CallBack_Test.py"
