4b4df8f5c0a02a6b0091ba3befb4726fbcd04eb2
sureshpotru-BP <154581462+sureshpotru-BP@users.noreply.github.com>
2024-05-29 19:34:51 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,10 +7,10 @@ data "archive_file" "init" {
 
 resource "aws_lambda_function" "example" {
   filename         = "${path.module}/lambda/CallBack_Test.zip"
-  function_name    = "CallbackTest"
+  function_name    = "CallBack_AmazonConnect"
   #role             = aws_iam_role.lambda_exec.arn
   role             = "arn:aws:iam::008426199026:role/bpcm-lambda-dynamodb"
-  handler          = "CallBack_Test.lambda_handler"
+  handler          = "CallBack_AmazonConnect.lambda_handler"
   #filename         = "CallBack_Test.py"
   #source_code_hash = data.archive_file.init.output_base64sha256
   runtime          = "python3.11"
